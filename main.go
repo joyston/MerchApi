1c6d31ddfb9312d4d229b79b98fcad1b3c7a82d6
DESKTOP-H87TPR2\ASUS <joystonf@gmail.com>
2025-07-10 23:48:59 +0530

    -Practiced RESTful ApI implementation on my Merch System

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Merch struct {
+	Name  string  `json: "name"`
+	Price float64 `json: "price"`
+	Type  string  `json: "type"`
+}
+
+var Data = []Merch{
+	{Name: "Rooted in christ", Price: 499, Type: "Round Neck"},
+	{Name: "One Way", Price: 699, Type: "Polo"},
+}
+
+func GetAllMerch(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, Data)
+}
+
+func GetMerchByName(c *gin.Context) {
+	name := c.Param("name")
+
+	for _, m := range Data {
+		if m.Name == name {
+			c.IndentedJSON(http.StatusFound, m)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Product does not exist"})
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/allmerch", GetAllMerch)
+	router.GET("/merch/:name", GetMerchByName)
+
+	//Using anonymous function
+	router.POST("/merch", func(c *gin.Context) {
+		var newMerch Merch
+
+		err := c.BindJSON(&newMerch)
+
+		if err == nil {
+			Data = append(Data, newMerch)
+			c.IndentedJSON(http.StatusCreated, Data)
+		} else {
+			return
+		}
+	})
+
+	router.Run("localhost:8080")
+}
